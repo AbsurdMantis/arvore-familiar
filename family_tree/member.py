9d0756af4fbd7ba25b746af6901a958c951e0b2b
raphael-holmes <raphaelbholmes@gmail.com>
2023-02-16 02:57:37 -0300

    Class init and test member

---
+++ family_tree/member.py
@@ -0,0 +1,16 @@
+import enum
+
+class Gender(enum.Enum):
+    male = "Male"
+    female = "Female"
+
+class Member:
+    
+    def __init__(self, id, name, gender):
+        self.id = id
+        self.name = name
+        self.gender = Gender(gender)
+        self.mother = None
+        self.father = None
+        self.children = []
+        
--- tests/unit/test_members.py
+++ tests/unit/test_members.py
@@ -3,5 +3,20 @@ from family_tree.member import Member
 
 class TestMember(TestCase):
     
-    def setUp(self):
-        self.member
+    def setUp(self): 
+        self.member = Member(1, "Lucas", "Male")
+    
+    def test_initialization(self):
+        self.assertEqual(isinstance(self.member, Member), True)
+        
+        
+        self.assertEqual(self.member.id, 1)
+        self.assertEqual(self.member.name, "Lucas")
+        self.assertEqual(self.member.gender.value, "Male")
+        self.assertEqual(self.member.mother, None)
+        self.assertEqual(self.member.father, None)
+        #self.assertEqual(self.member.spouse, None)
+        self.assertEqual(self.member.children, [])
+        
+        #edge case
+        self.assertRaises(ValueError, Member, 2, "SomeOther", "Queer")
