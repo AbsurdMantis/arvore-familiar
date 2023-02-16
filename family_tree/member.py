6af71cb551a385f8607b003169bcf7454c64ed03
raphael-holmes <raphaelbholmes@gmail.com>
2023-02-16 04:43:00 -0300

    Member unit testing

--- family_tree/member.py
+++ family_tree/member.py
@@ -12,5 +12,40 @@ class Member:
         self.gender = Gender(gender)
         self.mother = None
         self.father = None
+        self.spouse = None
         self.children = []
         
+    def set_mother(self, mother):
+        if not isinstance(mother, Member):
+            raise ValueError('Invalid value for mother')
+        if mother.gender != Gender.female:
+            raise ValueError(
+                'Invalid gender value for mother.'
+                'Mother should be a Female'
+            )
+        self.mother = mother
+    
+    def set_father(self, father):
+        if not isinstance(father, Member):
+            raise ValueError('Invalid value for father')
+        if father.gender != Gender.male:
+            raise ValueError(
+                'Invalid gender value for father.'
+                'Father should be a Male'
+            )
+        self.father = father
+    
+    def set_spouse(self, spouse):
+        if not isinstance(spouse, Member):
+            raise ValueError('Invalid value for spouse')
+        if self.gender == spouse.gender:
+            raise ValueError(
+                'Invalid gender value for spouse.'
+                'Spouse and member cannot have the same gender.'
+            )
+        self.spouse = spouse
+    
+    def add_child(self, child):
+        if not isinstance(child, Member):
+            raise ValueError('Invalid value for child')
+        self.children.append(child)
--- tests/unit/test_members.py
+++ tests/unit/test_members.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from family_tree.member import Member
+from family_tree.member import Member, Gender
 
 class TestMember(TestCase):
     
@@ -12,11 +12,61 @@ class TestMember(TestCase):
         
         self.assertEqual(self.member.id, 1)
         self.assertEqual(self.member.name, "Lucas")
-        self.assertEqual(self.member.gender.value, "Male")
+        self.assertEqual(self.member.gender, Gender.male)
         self.assertEqual(self.member.mother, None)
         self.assertEqual(self.member.father, None)
-        #self.assertEqual(self.member.spouse, None)
+        self.assertEqual(self.member.spouse, None)
         self.assertEqual(self.member.children, [])
         
         #edge case
         self.assertRaises(ValueError, Member, 2, "SomeOther", "Queer")
+        
+    
+    def test_set_mother(self):
+        mother_demo_a = "mother_demo_a"
+        mother_demo_b = Member(2, "MotherDemoB", "Male")
+        mother_demo_c = Member(3, "Mom", "Female")
+
+        # error case
+        self.assertRaises(ValueError, self.member.set_mother, mother_demo_a)
+        self.assertRaises(ValueError, self.member.set_mother, mother_demo_b)
+
+        # success case
+        self.member.set_mother(mother_demo_c)
+        self.assertEqual(self.member.mother.name, "Mom")
+        self.assertEqual(self.member.mother.gender, Gender.female)
+        
+    def test_set_father(self):
+        father_demo_a = "family_demo_a"
+        father_demo_b = Member(2, "FatherDemoB", "Female")
+        father_demo_c = Member(3, "Dad", "Male")
+
+        # error case
+        self.assertRaises(ValueError, self.member.set_father, father_demo_a)
+        self.assertRaises(ValueError, self.member.set_father, father_demo_b)
+
+        # success case
+        self.member.set_father(father_demo_c)
+        self.assertEqual(self.member.father.name, "Dad")
+        self.assertEqual(self.member.father.gender, Gender.male)
+    
+    def test_set_spouse(self):
+        spouse_demo_a = "spouse_demo_a"
+        spouse_demo_b = Member(2, "SpouseDemoB", "Male")
+        spouse_demo_c = Member(3, "Wife", "Female")
+
+        # error case
+        self.assertRaises(ValueError, self.member.set_spouse, spouse_demo_a)
+        self.assertRaises(ValueError, self.member.set_spouse, spouse_demo_b)
+
+        # success case
+        self.member.set_spouse(spouse_demo_c)
+        self.assertEqual(self.member.spouse.name, "Wife")
+        self.assertEqual(self.member.spouse.gender, Gender.female)
+
+    def test_add_child(self):
+        child_demo_a = "child_demo_a"
+        child_demo_b = Member(4, "Daughter", "Female")
+
+        # error case
+        self.assertRaises(ValueError, self.member.add_child, child_demo_a)
